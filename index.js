494de1695ec0ec1dc8fae86e1409e72b3211a3a6
brianhkang1@gmail.com <brianhkang1@gmail.com>
2018-06-11 16:59:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 function theBeatlesPlay(musician, instrument) {
   var randomarray = []
   
-  for (var i = 0; i < musician.length - 1; i++) {
+  for (var i = 0; i < musician.length; i++) {
     randomarray.push(`${musician[i]} plays ${instrument[i]}`)
   }
   return randomarray
+}
+
+function johnLennonFacts(array) {
+  var newarray = [] 
+  var i = 0
+  while (array[i]) {
+    newarray.push(array[i] + "!!!")
+    i++
+  }
+  return newarray
+}
+
+function iLoveTheBeatles(num) {
+  num = 0
+  var newarray = []
+
+  do {
+    newarray.push("I love the Beatles!")
+  } while (num < 15);
+  num++
+  
+  return newarray
 }
